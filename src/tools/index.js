7dc0582c7284589e7ebf58f489498ead2c954738
Nicolás Streri <nicolasstreri@gmail.com>
2021-06-17 19:57:09 -0300

    feat: HTML Code Sniffer was added

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,3 +1,19 @@
+"use strict";
 /**
- * When integrating a new accessibility testing tool, add export to this file.
+ * Note: When integrating a new accessibility testing tool, add export to this file.
  */
+const htmlcsRunner = require("./HTMLCodeSniffer/runner");
+
+exports.list = [htmlcsRunner];
+exports.compatibleWith = compatibleWith;
+
+/**
+ * Get runners compatible with a standard
+ * @param {String} standard
+ * @returns {Array} Array of Runner objects
+ */
+function compatibleWith(standard) {
+  return this.list.filter((runner) => {
+    return runner.allowedStandards.includes(standard);
+  });
+}
