659c2459b535c2fe5b70a23e23bd9f24dfc1211b
Nicolás Streri <nicolasstreri@gmail.com>
2021-06-17 22:40:19 -0300

    build: npm bin with simple usage

--- bin/myapp.js
+++ bin/myapp.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 "use strict";
-console.log("Hello!");
+const app = require("../src/app");
+const cliReport = require("../src/reporters/CLIReport");
+
+analyzeArguments();
+runApp();
+
+/**
+ * Parse the flags and arguments passed to the CLI
+ * @return {void}
+ */
+function analyzeArguments() {
+  //TODO Parse the flags and arguments passed to the CLI
+}
+
+/**
+ * Run the accessibility test on the page and generate the results
+ */
+async function runApp() {
+  app
+    .run("https://google.com", "WCAG2A", {
+      reporter: cliReport,
+    })
+    .then((results) => {
+      results = results.filter((result) => {
+        return result.type != "notice";
+      });
+      cliReport.reportFrom(results);
+    })
+    .catch((err) => {
+      cliReport.reportError(err.message);
+    });
+}
