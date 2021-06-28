7f2230633453b72a1036a37a4fdac45b4aef49c7
Nicolás Streri <nicolasstreri@gmail.com>
2021-06-27 22:01:29 -0300

    refactor: Conversion of Axe codes to standard codes

--- src/tools/Axe/transformer.js
+++ src/tools/Axe/transformer.js
@@ -2,13 +2,8 @@
  * Note:
  *  For more information on the format of the Axe result, see https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#results-object
  */
-
+const { map } = require("./axeToStandardCodeMap");
 exports.convert = convert;
-
-function mapCodeToStandardCode(axeCode) {
-  //TODO Reimplement it after analysing the standard code format.
-  return axeCode;
-}
 
 function getElement(selectors, separator) {
   //Extract from https://github.com/dequelabs/axe-cli/blob/3363acc6bb9d62cba254d38a7fb324807543725e/lib/utils.js#L75
@@ -18,12 +13,12 @@ function getElement(selectors, separator) {
     .join(separator);
 }
 
-function convertResults(axeResults, type) {
+function convertResults(axeResults, type, standard) {
   let results = [];
   for (const issue of axeResults) {
     for (const node of issue.nodes) {
       results.push({
-        code: mapCodeToStandardCode(issue.id),
+        code: map(issue.id, standard),
         element: getElement(node.target),
         message: `${issue.description}. ${issue.help}.`,
         type: type,
@@ -33,26 +28,19 @@ function convertResults(axeResults, type) {
   return results;
 }
 
-function convertViolations(violations) {
-  return convertResults(violations, "error");
-}
-
-function convertIncomplete(incomplete) {
-  return convertResults(incomplete, "warning");
-}
-
 /**
  * Convert Axe results to standard results
  * @param {Array} axeResults Arrays of Axe results.
+ * @param {String} standard Accesibility Standard used
  * @returns {Array} Array of standard results
  */
-function convert(axeResults) {
+function convert(axeResults, standard) {
   const violations = axeResults[0].violations;
   const incomplete = axeResults[0].incomplete;
 
   let results = [];
-  results.push(...convertIncomplete(incomplete));
-  results.push(...convertViolations(violations));
+  results.push(...convertResults(incomplete, "warning", standard));
+  results.push(...convertResults(violations, "error", standard));
 
   return results;
 }
